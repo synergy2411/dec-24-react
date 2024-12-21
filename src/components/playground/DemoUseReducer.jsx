59fb7bf97cc14c87c875a5bac5c179969b3c975c
Sumit Khandelwal <synergy2411@outlook.com>
2024-12-21 11:53:18 +0530

    result added

--- src/components/playground/DemoUseReducer.jsx
+++ src/components/playground/DemoUseReducer.jsx
@@ -2,25 +2,67 @@ import { useReducer } from "react";
 
 function reducerFn(state, action) {
   if (action.type === "INCREMENT") {
-    return { counter: state.counter + 1 };
+    return { ...state, counter: state.counter + 1 };
+  } else if (action.type === "DECREMENT") {
+    return { ...state, counter: state.counter - 1 };
+  } else if (action.type === "ADD_COUNTER") {
+    return { ...state, counter: state.counter + action.payload };
+  } else if (action.type === "STORE_RESULT") {
+    return { ...state, result: [state.counter, ...state.result] };
   }
+
   return state;
 }
 
 const DemoUseReducer = () => {
-  const [state, dispatch] = useReducer(reducerFn, { counter: 0 });
+  const [state, dispatch] = useReducer(reducerFn, { counter: 0, result: [] });
 
   return (
     <div className="text-center">
       <h1>Use Reducer Demo</h1>
       <p className="display-4">Counter: {state.counter}</p>
       <br />
-      <button
-        className="btn btn-primary"
-        onClick={() => dispatch({ type: "INCREMENT" })}
-      >
-        Increase
-      </button>
+      <div className="mb-4">
+        <button
+          className="btn btn-primary"
+          onClick={() => dispatch({ type: "INCREMENT" })}
+        >
+          Increase
+        </button>
+        <button
+          className="btn btn-secondary"
+          onClick={() => dispatch({ type: "DECREMENT" })}
+        >
+          Decrease
+        </button>
+        <button
+          className="btn btn-info"
+          onClick={() => dispatch({ type: "ADD_COUNTER", payload: 10 })}
+        >
+          Add (10)
+        </button>
+      </div>
+      <br />
+      <div className="row">
+        <div className="offset-4 col-4">
+          <div className="d-grid">
+            <button
+              className="btn btn-warning"
+              onClick={() => dispatch({ type: "STORE_RESULT" })}
+            >
+              Store Result
+            </button>
+          </div>
+          <br />
+          <ul className="list-group">
+            {state.result.map((r) => (
+              <li className="list-group-item mb-2" key={r}>
+                {r}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
